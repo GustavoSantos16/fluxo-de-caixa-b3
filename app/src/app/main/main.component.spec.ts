451ed5b60306b78d3b95a59bad313b43877bd75f
Gustavo dos Santos <gustavogugueira@gmail.com>
2024-07-13 21:08:40 -0300

    feat: add tests

--- app/src/app/main/main.component.spec.ts
+++ app/src/app/main/main.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
@@ -8,7 +8,8 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ MainComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   });
@@ -21,5 +22,34 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a transaction', () => {
+    component.description = 'Teste Entrada';
+    component.value = 1000.00;
+    component.type = 'entrada';
+    component.date = '2024-07-12';
+    component.addTransaction();
+
+    expect(component.transactions.length).toBe(1);
+    expect(component.transactions[0].description).toBe('Teste Entrada');
+    expect(component.transactions[0].value).toBe(1000);
+    expect(component.transactions[0].type).toBe('entrada');
+    expect(component.transactions[0].date).toBe('2024-07-12');
+  });
+
+  it('should filter transactions by month and year', () => {
+    component.transactions = [
+      { description: 'Entrada 1', value: 1000, type: 'entrada', date: '2024-07-01' },
+      { description: 'Saída 1', value: 500, type: 'saida', date: '2024-07-02' },
+      { description: 'Entrada 2', value: 2000, type: 'entrada', date: '2024-06-01' }
+    ];
+
+    component.currentMonth = new Date('2024-07-01');
+    const filteredTransactions = component.filteredTransactions;
+
+    expect(filteredTransactions.length).toBe(2);
+    expect(filteredTransactions[0].description).toBe('Entrada 1');
+    expect(filteredTransactions[1].description).toBe('Saída 1');
   });
 });
--- app/src/app/services/currencies.service.spec.ts
+++ app/src/app/services/currencies.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CurrenciesService } from './currencies.service';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CurrenciesService]
+    });
     service = TestBed.inject(CurrenciesService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch currency data', () => {
+    const dataJson = {
+      USDBRL: { code: 'USD', codein: 'BRL', name: 'Dólar Americano/Real Brasileiro', high: '5.25', low: '5.20', varBid: '0.01', pctChange: '0.19', bid: '5.23', ask: '5.24', timestamp: '1623190441', create_date: '2023-07-12 11:00:00' }
+    };
+
+    service.getCurrencies('USD-BRL').subscribe(data => {
+      expect(data).toEqual(dataJson);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}USD-BRL`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dataJson);
   });
 });
